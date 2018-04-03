e64436fcc1f2cd830771f325c30a4407b32176f9
Denys Vitali <denys@denv.it>
2018-04-03 22:50:30 +0200

    Add Gogs link

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -15,6 +15,8 @@ use rocket;
 
 #[get("/")]
 pub fn main() -> content::Html<String> {
+    let mut s : String = String::from(env!("GIT_HASH"));
+    s.truncate(7);
     return content::Html(format!(r#"<!DOCTYPE html>
     <html>
     <head>
@@ -29,12 +31,31 @@ pub fn main() -> content::Html<String> {
     <body>
         <h1>GTFS Server</h1>
         <p>
-            Commit: <a href="https://github.com/denysvitali/gtfs-server/commit/{0}">{0}</a> - {1}
+            Commit ({2} - {1}) {0}
         </p>
+            <table>
+                <tr>
+                    <td>
+                        GitHub
+                    </td>
+                    <td>
+                        <a href="https://github.com/denysvitali/gtfs-server/commit/{0}">{2}</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Gogs
+                    </td>
+                    <td>
+                        <a href="https://git.ded1.denv.it/shrug/gtfs-server/commit/{0}">{2}</a>
+                    </td>
+                </tr>
+            </table>
     </body>
     </html>
     "#,
-                   env!("GIT_HASH"),
-                   env!("BUILD_DATE")
+        env!("GIT_HASH"),
+        env!("BUILD_DATE"),
+        s
     ));
 }
